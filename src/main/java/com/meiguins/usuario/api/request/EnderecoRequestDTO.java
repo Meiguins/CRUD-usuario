b093598f3f25ca503a6319bbe147b095f373b990
Kauan Meiguins <112512325+Meiguins@users.noreply.github.com>
2024-05-19 16:50:52 -0300

    add class's requestDTO

---
+++ src/main/java/com/meiguins/usuario/api/request/EnderecoRequestDTO.java
@@ -0,0 +1,23 @@
+package com.meiguins.usuario.api.request;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode
+public class EnderecoRequestDTO {
+    private String rua;
+
+    private Long numero;
+
+    private String bairro;
+
+    private String complemento;
+
+    private String cidade;
+
+    private String cep;
+}
