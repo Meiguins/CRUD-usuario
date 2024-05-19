87a007882212bfa7805640b16638b2f7fba74785
Kauan Meiguins <112512325+Meiguins@users.noreply.github.com>
2024-05-19 16:49:03 -0300

    add class's responseDTO

---
+++ src/main/java/com/meiguins/usuario/api/response/UsuarioResponseDTO.java
@@ -0,0 +1,12 @@
+package com.meiguins.usuario.api.response;
+
+public record UsuarioResponseDTO (String id,
+
+                                  String nome,
+
+                                  String email,
+
+                                  String documento,
+
+                                  EnderecoResponseDTO endereco) {
+}
